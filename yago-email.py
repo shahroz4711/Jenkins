4e9dc3c722e8c84be1a88a3e1291c5173f716d72
Shahroz samar <157927605+shahroz4711@users.noreply.github.com>
2024-06-27 20:30:47 -0700

    Update yago-email.py

--- yago-email.py
+++ yago-email.py
@@ -2,7 +2,7 @@ dimport yagmail
 
 # Replace with your email credentials and recipient
 yag = yagmail.SMTP('coursesinfo9@gmail.com', 'ubgdmmxzdycfheew')
-contents = "ki hal aa thk hu "
+contents = "ki hal aa thk hu "er
 yag.send('iamok7372@gmail.com', 'Changing dffdff dffdfdffilfdfdfddffddfdfdffdffdffde compdfdfdfdf dfldfdfdfdfdfetely', contents)
 SDDS
 print("email has been sent")
